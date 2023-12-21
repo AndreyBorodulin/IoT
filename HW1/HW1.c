914e2e1a53a1e7fc3f1e7fb600badc6f42e04c1c
AndreyBorodulin <fnm1005@gmail.com>
2023-12-21 23:22:29 +0300

    First commit

---
+++ HW1/HW1.c
@@ -0,0 +1,10 @@
+// ДЗ1
+#include <stdio.h>
+
+int main()
+{
+   printf("Hello world!\n");
+   printf("\nLet's\n   go\n     to walk\n\n");
+   printf("\t   *\n\t  ***\n\t *****\n\t*******\n\t HH HH\n\t ZZZZZ\n");
+   return 0;
+}
